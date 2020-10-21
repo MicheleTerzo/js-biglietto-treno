d4fd35ca8aa63076ef0b5bba248d98fc5de48ad1
Michele Terzo <micheleterzo98@gmail.com>
2020-10-21 10:14:43 +0200

    Minor Fixes

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var distance = parseInt(prompt("Quanti chilometri si desidera percorrere?"));
 var age = parseInt(prompt("Si prega di inserire l'età:"));
 
-document.getElementById("dis").innerHTML = (distance) + (" Km");
 document.getElementById("eta").innerHTML = (age) + (" anni");
+document.getElementById("dis").innerHTML = (distance) + (" Km");
 
-var price = (distance * 0.21);
-var minordiscount = price * 0.2;
-var majordiscount = price * 0.4;
+var price = (distance * 0.21); //prezzo unitario per chilometro
+var minordiscount =  0.2; //sconto del 20%
+var majordiscount =  0.4; //sconto del 40%
 
-if (age <= 18) {
-    document.getElementById("costo").innerHTML = (price - minordiscount) + ("€");
-} else if (age >= 65) {
-    document.getElementById("costo").innerHTML = (price - majordiscount) + ("€");
-} else {
-    document.getElementById("costo").innerHTML = (price) + ("€");
+
+if (age < 18) { //Se il viaggiatore è minorenne
+    price -= price * minordiscount;
+    document.getElementById("costo").innerHTML = price.toFixed(2) + ("€");
+} else if (age >= 65) { //Se il viaggiatore è over65
+    price -= price * majordiscount;
+    document.getElementById("costo").innerHTML = price.toFixed(2)  + ("€");
+} else { //Se il viaggiatore non ha diritto a sconto
+    document.getElementById("costo").innerHTML = price.toFixed(2) + ("€");
 }  
   
 
